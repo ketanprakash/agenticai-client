ee973935aa753634568798107b9ab3266398cfb3
Suyash Vikram Singh <suyashvikramsingh2002@gmail.com>
2025-07-26 17:07:13 +0000

    Added all field except location

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,50 @@ import Image from "next/image";
 import { RxAvatar } from "react-icons/rx";
 import Select from 'react-select'
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+const optionsType = [
+  {
+    value: 'fullDay',
+    label: 'Full Day',
+  },
+  {
+    value: 'morning',
+    label: 'Morning',
+  },
+  {
+    value: 'afternoon',
+    label: 'Afternoon',
+  },
+  {
+    value: 'evening',
+    label: 'Evening',
+  },
+  {
+    value: 'night',
+    label: 'Night',
+  }
+]
+const optionsPeople = [
+  {
+    value: 'family',
+    label: 'Family',
+
+  },
+  {
+    value: 'friends',
+    label: 'Friends',
+  },
+  {
+    value: 'colleagues',
+    label: 'Colleagues',
+  },
+  {
+    value: 'solo',
+    label: 'Solo',
+  },
+  {
+    value: 'couple',
+    label: 'Couple',
+  }
 ]
 
 const styles = {
@@ -41,10 +81,17 @@ const page = () => {
         />
       </div>
       <div className="flex flex-col pt-4 px-2 gap-4">
-        <Select options={options} styles={styles} />
-        <Select options={options} styles={styles} />
-        <Select options={options} styles={styles} />
-        <Select options={options} styles={styles} />
+        <Select options={optionsType} styles={styles} placeholder="Type of Plan" />
+        <Select options={optionsPeople} styles={styles} placeholder="Type of People" />
+        <div className="border border-gray-500 rounded-xl bg-white w-full" >
+          <input placeholder="Number of People" className="py-1 px-1.5 m-[2px] w-full outline-none" type="number"/>
+        </div>
+        <div className="border border-gray-500 rounded-xl bg-white w-full" >
+          <input placeholder="Date" id="date" className="py-1 px-1.5 m-[2px] w-full outline-none" type="date"/>
+        </div>
+        <div className="border border-gray-500 rounded-xl bg-white w-full" >
+          <input placeholder="Time" id="time" className="py-1 px-1.5 m-[2px] w-full outline-none" type="time"/>
+        </div>
       </div>
       <div className="fixed bottom-0 left-0 right-0 p-4 shadow-md">
         <button className="bg-orange-700 cursor-pointer text-white rounded-[12px] hover:bg-orange-800 transition-colors w-full p-3">
